7ba562771676bd7dd7254804c15df416fa304d0d
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2023-04-20 13:50:40 +0900

    Fix: p2のファイルも追加

---
+++ task1/p2/complex-test.c
@@ -0,0 +1,120 @@
+﻿#include <stdio.h>
+
+struct  complex_number {          /* 複素数を表現する構造体 */
+    double	real;    /* 実数部 */
+    double	imag;    /* 虚数部 */
+};
+
+/* 複素数の加算 */
+struct complex_number
+complex_number_add(struct complex_number a, struct complex_number b)
+{
+    struct complex_number result;
+
+    result.real = a.real + b.real;
+    result.imag = a.imag + b.imag;
+
+    return result;
+}
+
+/* 複素数の減算 */
+struct complex_number
+complex_number_sub(struct complex_number a, struct complex_number b)
+{
+    struct complex_number result;
+
+    result.real = a.real - b.real;
+    result.imag = a.imag - b.imag;
+
+    return result;
+}
+
+/* 複素数の乗算 */
+struct complex_number
+complex_number_mul(struct complex_number a, struct complex_number b)
+{
+    struct complex_number result;
+
+    result.real = a.real * b.real - a.imag * b.imag;
+    result.imag = a.real * b.imag + a.imag * b.real;
+
+    return result;
+}
+
+/* 複素数の除算 */
+struct complex_number
+complex_number_div(struct complex_number a, struct complex_number b)
+{
+    struct complex_number result = { 0.0, 0.0 };
+    double d;
+
+    d = b.real * b.real + b.imag * b.imag;
+    if (d == 0.0) {
+	printf("divided by zero\n");
+	return result;
+    }
+
+    result.real = (a.real * b.real + a.imag * b.imag)/d;
+    result.imag = (- a.real * b.imag + a.imag * b.real)/d;
+
+    return result;
+}
+
+/* 複素数の表示 */
+void
+complex_number_print(struct complex_number a)
+{
+    if (a.imag < 0)
+	printf("%f%fi", a.real, a.imag);
+    else
+	printf("%f+%fi", a.real, a.imag);
+}
+
+void
+calc_test(struct complex_number a, struct complex_number b, char optype)
+{
+    struct complex_number c;
+
+    switch (optype) {
+    case '+':
+	c = complex_number_add(a, b);
+	break;
+    case '-':
+	c = complex_number_sub(a, b);
+	break;
+    case '*':
+	c = complex_number_mul(a, b);
+	break;
+    case '/':
+	c = complex_number_div(a, b);
+	break;
+    default:
+	printf("unknown operation type %c\n", optype);
+	return;
+	break;
+    }
+
+    complex_number_print(a);
+    printf(" %c ", optype);
+    complex_number_print(b);
+    printf(" = ");
+    complex_number_print(c);
+    printf("\n");
+}
+
+int
+main(void)
+{
+    struct complex_number a, b;		/* 構造体complex_number型の変数の宣言 */
+
+    a.real = 3.0; a.imag = 4.0;    	/* 変数aの値の設定 */
+    b.real = 1.0; b.imag = 2.0;    	/* 変数bの値の設定 */
+
+    /* 2つの複素数の四則演算結果を表示 */
+    calc_test(a, b, '+');
+    calc_test(a, b, '-');
+    calc_test(a, b, '*');
+    calc_test(a, b, '/');
+
+    return 0;
+}
---
+++ task1/p2/rat-num.c
@@ -0,0 +1,183 @@
+﻿/*
+ * rat-num.c
+ * rational number calculation program
+ */
+#include <stdio.h>
+
+/* 有理数(分数)を表現する構造体 */
+typedef struct rational_number
+{
+    int numer; /* 分子 (numerator) */
+    int denom; /* 分母 (denominator) */
+} rational_number;
+
+/* 有理数(分数)の加算 */
+/* 2つの引数で与えられた分数の和を求めて返す */
+rational_number
+rational_number_add(rational_number a, rational_number b)
+{
+    rational_number result;
+
+    result.numer = a.numer * b.denom + b.numer * a.denom;
+    result.denom = a.denom * b.denom;
+
+    return result;
+}
+
+/* 有理数(分数)の減算 */
+/* 2つの引数で与えられた分数の差を求めて返す */
+rational_number
+rational_number_sub(rational_number a, rational_number b)
+{
+    rational_number result;
+
+    result.numer = a.numer * b.denom - b.numer * a.denom;
+    result.denom = a.denom * b.denom;
+
+    return result;
+}
+
+/* 有理数(分数)の乗算 */
+/* 2つの引数で与えられた分数の積を求めて返す */
+rational_number
+rational_number_mul(rational_number a, rational_number b)
+{
+    rational_number result;
+
+    result.numer = a.numer * b.numer;
+    result.denom = a.denom * b.denom;
+
+    return result;
+}
+
+/* 有理数(分数)の除算 */
+/* 2つの引数で与えられた分数の商を求めて返す */
+rational_number
+rational_number_div(rational_number a, rational_number b)
+{
+    rational_number result;
+
+    result.numer = a.numer * b.denom;
+    result.denom = a.denom * b.numer;
+
+    return result;
+}
+
+/* ユークリッドの互除法 */
+int gcd(int a, int b)
+{
+    int i;
+
+    i = a % b;
+    if (i == 0)
+    {
+        return b;
+    }
+    else
+    {
+        a = b;
+        b = i;
+        return gcd(a, b);
+    }
+}
+
+/* 有理数(分数)の既約化 */
+/* 引数で与えられた分数の既約分数を求めて返す */
+rational_number
+rational_number_reduced(rational_number a)
+{
+    int i = gcd(a.numer, a.denom);
+
+    if (i != 0)
+    {
+        a.numer = a.numer / i;
+        a.denom = a.denom / i;
+    }
+
+    if (a.denom < 0)
+    {
+        a.numer = (-1) * a.numer;
+        a.denom = (-1) * a.denom;
+    }
+
+    return a;
+}
+
+/* 有理数(分数)の表示 */
+void rational_number_print(rational_number r)
+{
+    /*
+     * "[ 分子/分母 ]" の形式で表示
+     *
+     * ただし、
+     * 0のときはそのまま "0"と表示
+     * 1のときはそのまま "1"と表示
+     */
+
+    r = rational_number_reduced(r); /* 表示前に既約化 */
+
+    if (r.numer == 0)
+        printf("0");
+    else if (r.numer == 1 && r.denom == 1)
+        printf("1");
+    else
+        printf("[ %d/%d ]", r.numer, r.denom);
+}
+
+/* ここから先は動作テスト用の関数 */
+
+void test_rational_number_calc(rational_number a, rational_number b, char optype)
+{
+    rational_number c;
+
+    switch (optype)
+    {
+    case '+':
+        c = rational_number_add(a, b);
+        break;
+    case '-':
+        c = rational_number_sub(a, b);
+        break;
+    case '*':
+        c = rational_number_mul(a, b);
+        break;
+    case '/':
+        c = rational_number_div(a, b);
+        break;
+    default:
+        printf("unknown operation type %c\n", optype);
+        return;
+        break;
+    }
+
+    /* 計算内容の表示 */
+    rational_number_print(a);
+    printf(" %c ", optype);
+    rational_number_print(b);
+    printf(" = ");
+    rational_number_print(c);
+    printf("\n");
+}
+
+int main(void)
+{
+    rational_number a, b; /* 構造体 rational_number型の変数の宣言 */
+
+    /* キーボードから4つの数を入力 */
+    printf("input number (a.numer) = ");
+    scanf("%d", &a.numer);
+    printf("input number (a.denom) = ");
+    scanf("%d", &a.denom);
+    printf("input number (b.numer) = ");
+    scanf("%d", &b.numer);
+    printf("input number (b.denom) = ");
+    scanf("%d", &b.denom);
+
+    /* 2つの有理数(分数)の四則演算結果を表示 */
+    test_rational_number_calc(a, b, '+');
+    test_rational_number_calc(a, b, '-');
+    test_rational_number_calc(a, b, '*');
+    test_rational_number_calc(a, b, '/');
+
+    return 0;
+}
